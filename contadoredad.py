159796e0f07c15431ce09adfbfcfdf11b1add5eb
Gabriel Feltrin <gabrielfeltrin.wae@gmail.com>
2026-08-25 17:28:50 -0300

    Calculadordepropina.py

--- contadoredad.py
+++
@@ -1,4 +0,0 @@
-Edad = 40
-Dias_vividos = Edad * 365
-print("Tengo", Edad, "años de edad que euequivale a",
-      Dias_vividos, "dias de vida.")
--- repartidor.py
+++
@@ -1,4 +0,0 @@
-Dulces = 125
-Niños = 5
-print("Corresponden", (Dulces // Niños), "por cada niño",
-      "y sobran", (Dulces % Niños))
