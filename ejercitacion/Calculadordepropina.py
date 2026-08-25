19eeee00e141109c11ea5957b465d8a5f7b003ca
Gabriel Feltrin <gabrielfeltrin.wae@gmail.com>
2026-08-25 13:38:31 -0300

    Subiendo Primeras Ejercitaciones

---
+++ ejercitacion/Calculadordepropina.py
@@ -0,0 +1,6 @@
+Costo_de_la_cena = 100
+
+Propina = Costo_de_la_cena * 0.1
+Iva = Costo_de_la_cena * 0.21
+print("La propina equivale a", Propina)
+print("El IVA equivale a", Iva)
---
+++ ejercitacion/repartidor.py
@@ -0,0 +1,4 @@
+Dulces = 125
+Niños = 5
+print("Corresponden", (Dulces // Niños), "por cada niño",
+      "y sobran", (Dulces % Niños))
