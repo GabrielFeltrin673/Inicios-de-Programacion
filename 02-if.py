1850e6d51034f1c314ee257dc2ae995bdf1ad264
Gabriel Feltrin <gabrielfeltrin.wae@gmail.com>
2026-08-26 20:31:38 -0300

    comparadores y condicionales

---
+++ 02-if.py
@@ -0,0 +1,10 @@
+edad = 70
+if edad >= 65:
+    print("Eres adulto mayor")
+elif edad >= 18:
+    print("Eres mayor de edad")
+    print("listo")
+else:
+    edad < 18
+    print("Eres menor de edad")
+    print("listo")
---
+++ 03-comparadores-logicos.py
@@ -0,0 +1,7 @@
+gas = False
+encendido = False
+edad = 19
+
+
+if not (gas and encendido) and edad > 17:
+    print("No puedes ni debes manejar el auto.")
