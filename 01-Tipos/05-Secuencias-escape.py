650e4a4dead9504a0754e38fc9ec0aa30d4a5825
Gabriel Feltrin <gabrielfeltrin.wae@gmail.com>
2026-08-31 16:01:09 -0300

    agregando tipos 05 06 y 07

---
+++ 01-Tipos/05-Secuencias-escape.py
@@ -0,0 +1,6 @@
+# secuencias de escape
+# \n salto de linea
+# \t tabulador
+# \" comillas dobles
+# \' comillas simples
+# \\ barra invertida
---
+++ 01-Tipos/06-Numeros.py
@@ -0,0 +1,16 @@
+numero = 2  # entero o integer
+decimal = 2.5  # decimal o float
+imagginario = 3 + 5j  # numero complejo
+
+numero = numero + 2
+print(numero)  # 4
+# o también se puede escribir como: numero += 2
+# mismo caso para el resto de operaciones aritméticas: -=, *=, /=, //=, %=, **=
+
+print(1 + 3)  # suma
+print(3 - 1)  # resta
+print(2 * 4)  # multiplicación
+print(10 / 2)  # división
+print(10 // 3)  # división entera #3
+print(10 % 3)  # módulo # resto de la división #1
+print(2 ** 3)  # potencia
