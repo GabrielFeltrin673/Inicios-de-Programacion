cc7a7c7376772059b9beb70dc120d7586f745d92
Gabriel Feltrin <gabrielfeltrin.wae@gmail.com>
2026-09-02 20:17:29 -0300

    Finalizando Tipos Comenzando control de flujo

---
+++ 01-Tipos/09-ConversionTipos.py
@@ -0,0 +1,11 @@
+x = input ("Ingrese un número: ")
+x = int(x) # Convertir a entero
+x = float(x) # Convertir a flotante o decimal
+x = str(x) # Convertir a cadena de texto
+x = bool(x) # Convertir a booleano
+
+print(bool(""))
+print(bool(0))
+print(bool(None))
+print(bool(" "))
+print(bool("0"))
