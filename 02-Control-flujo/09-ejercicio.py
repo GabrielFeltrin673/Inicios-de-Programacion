a0d2ec0b76fd818a7948ff1a27fb9a5806ac27f6
Gabriel Feltrin <gabrielfeltrin.wae@gmail.com>
2026-09-09 17:24:18 -0300

    tratando de hacer la segunda calculadora

--- 02-Control-flujo/09-ejercicio.py
+++ 02-Control-flujo/09-ejercicio.py
@@ -1,7 +1,10 @@
-#crear una aplicacion interactiva
-#esta aplicacion tiene que verificar si ya ingresamos un numero
-#si no lo ingreso hay que pediirle que lo ingrese. Luego de eso hay que pedirle que ingrese una operacion
-#si ya lo ingreso, hay qu epedirle que ingrese una operacion    
-#ona vez que ingresa la operacion hay que pedirle que ingrese un segundo numero.
-#una vez que hace eso, debemos mostrar el resultado, cuando esto suceda, la guardamos como el primer 
+# crear una aplicacion interactiva
+# esta aplicacion tiene que verificar si ya ingresamos un numero
+# si no lo ingreso hay que pediirle que lo ingrese. Luego de eso hay
+#  que pedirle que ingrese una operacion
+# si ya lo ingreso, hay qu epedirle que ingrese una operacion
+# ona vez que ingresa la operacion hay que pedirle que ingrese un
+#  segundo numero.
+# una vez que hace eso, debemos mostrar el resultado, cuando esto su
+# ceda, la guardamos como el primer
 # numero y le pedimos que ingrese otra operacion
